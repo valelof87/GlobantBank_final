282f44fae900f485b4fb2736ae78ae75a2c07866
valelof87 <valerialofrano87@gmail.com>
2018-10-10 11:28:37 -0300

    ordenar

--- java8stream/src/main/java/com/stream/java8/java8stream/App.java
+++ java8stream/src/main/java/com/stream/java8/java8stream/App.java
@@ -11,18 +11,26 @@ public class App
 	
 	public App() {
 		lista = new ArrayList<>();
-		lista.add("juan");
+		lista.add("Juan");
 		lista.add("Manuel");
-		lista.add("federico");
+		lista.add("Federico");
 		
 		numero= new ArrayList<>();
-		numero.add("1");
 		numero.add("2");
+		numero.add("1");
 	}
 	
 	//filtrar los que empeizan con M
 	public void filtrar() {
 		lista.stream().filter(x -> x.startsWith("M")).forEach(System.out::println );
+		
+		// o expresion lambda 
+		//lista.stream().filter(x -> x.startsWith("M")).forEach(x -> System.out.println(x );
+	}
+	
+	public void ordenar() {
+		lista.stream().sorted().forEach(System.out::println);
+		numero.stream().sorted().forEach(System.out::println);
 	}
 	
 	
@@ -30,5 +38,6 @@ public class App
     {
         App app = new App();
         app.filtrar();
+        app.ordenar();
     }
 }
